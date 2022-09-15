28c5cdf7abfa8e29b8f4e258033d1c7a622d0cf8
Carlos Osejo <caosejo@gmail.com>
2022-09-14 20:23:40 -0600

    Init project

---
+++ iAlbum_SwiftUI/iAlbum_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iAlbum_SwiftUI
+//
+//  Created by Carlos Osejo on 14/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iAlbum_SwiftUI/iAlbum_SwiftUI/iAlbum_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iAlbum_SwiftUIApp.swift
+//  iAlbum_SwiftUI
+//
+//  Created by Carlos Osejo on 14/9/22.
+//
+
+import SwiftUI
+
+@main
+struct iAlbum_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
